cacdf165baf2d4c08e3ea1504fcc9a34e174789c
Leeya Thercy <leeyathercy@gmail.com>
2024-06-12 13:40:04 -0500

    full jwt

---
+++ server/src/main/java/learn/solarfarm/SecretSigningKey.java
@@ -0,0 +1,21 @@
+
+package learn.solarfarm;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class SecretSigningKey {
+    private Key key;
+
+    public SecretSigningKey() {
+        this.key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
+
+    public Key getKey() {
+        return key;
+    }
+}
