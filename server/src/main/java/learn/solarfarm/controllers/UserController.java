737440a5d280af1c0a71c0e48abec9a460708da4
Leeya Thercy <leeyathercy@gmail.com>
2024-06-12 11:32:57 -0500

    Added jwt for logging in and creating user

--- server/src/main/java/learn/solarfarm/controllers/UserController.java
+++ server/src/main/java/learn/solarfarm/controllers/UserController.java
@@ -1,5 +1,8 @@
 package learn.solarfarm.controllers;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import learn.solarfarm.data.DataAccessException;
 import learn.solarfarm.domain.Result;
 import learn.solarfarm.domain.UserService;
@@ -25,7 +28,8 @@ public class UserController {
         Result<User> result = service.create(user);
 
         if (result.isSuccess()) {
-            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+            String jwt = getJwtStringFromUser(result.getPayload());
+            return new ResponseEntity<>(jwt, HttpStatus.CREATED);
         } else {
             return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
         }
@@ -39,9 +43,20 @@ public class UserController {
         if (existingUser == null) {
             return new ResponseEntity<>(List.of("That username does not exist"), HttpStatus.NOT_FOUND);
         } else if (existingUser.getPassword().equals(user.getPassword())) {
-            return new ResponseEntity<>(existingUser, HttpStatus.OK);
+            String jwt = getJwtStringFromUser(existingUser);
+
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(List.of("Invalid password"), HttpStatus.FORBIDDEN);
         }
+    }
+
+    private String getJwtStringFromUser(User user){
+        String jwt = Jwts.builder()
+                .claim("username", user.getUsername())
+                .claim("id", user.getId())
+                .signWith(Keys.secretKeyFor(SignatureAlgorithm.HS256))
+                .compact();
+        return jwt;
     }
 }
